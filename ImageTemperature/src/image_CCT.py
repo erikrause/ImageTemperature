f02ad52cd7ff662676b1befc1941dd0934dca961
Эркин Камилов <erkimkamilov@gmail.com>
2020-08-29 01:46:03 +0500

    Implemented vector operations. Implemented bias.

--- ImageTemperature/src/image_CCT.py
+++ ImageTemperature/src/image_CCT.py
@@ -6,19 +6,84 @@ from colormath.color_objects import XYZColor, sRGBColor, xyYColor
 from colormath.color_conversions import convert_color
 import time
 
-def CCT_Hernandez1999(lst_x, lst_y):
-    n = (lst_x-0.3366)/(lst_y-0.1735)
-    CCT = (-949.86315 + 6253.80338 * np.exp(-n / 0.92159) + 28.70599 * np.exp(-n / 0.20039) + 0.00004 * np.exp(-n / 0.07125))
+def sRGB_to_xyz(r,g,b):
+    """ Конвертирует цвет sRGB в XYZ спектр. Math source: http://www.brucelindbloom.com/index.html?Eqn_RGB_XYZ_Matrix.html """
 
-def get_image_CCT_np(rgb_img_array, mask=None):
+    M = np.array([[0.4124564, 0.3575761, 0.1804375],
+                  [0.2126729, 0.7151522, 0.0721750],
+                  [0.0193339, 0.1191920, 0.9503041]])
+    RGB = np.array([r, g, b])
 
-    x, y, _ = rgb_img_array.shape
+    return np.dot(M, RGB)
 
-    if mask == None:
-        mask = np.ones([x, y])
-    else:
-        mask = normalize_mask(mask)
+def xyz_arr_to_xy(xyz):
 
+    sum = np.sum(xyz, axis=-1)
+    sum[sum == 0] = 1   # Убирает деление на 0 в дальнейшем
+
+    xy_x = xyz[:, 0] / sum
+    xy_y = xyz[:, 1] / sum
+
+    return np.array([xy_x, xy_y])
+
+def CCT_Hernandez1999(xy_arr):
+    """ Возвращает коррерированную цветовую температуру по алгоритму Hernandez et al. 1999 """
+
+    n = (xy_arr[0]-0.3366)/(xy_arr[1]-0.1735)
+    CCT = (-949.86315 + 
+           6253.80338 * np.exp(-n / 0.92159) + 
+           28.70599 * np.exp(-n / 0.20039) + 
+           0.00004 * np.exp(-n / 0.07125))
+    #CCT[0] = 77000
+
+    n = np.where(CCT > 50000,
+                 (xy_arr[0] - 0.3356) / (xy_arr[1] - 0.1691),
+                 n)
+
+    CCT = np.where(CCT > 50000,
+                   36284.48953 + 0.00228 * np.exp(-n / 0.07861) +
+                   5.4535e-36 * np.exp(-n / 0.01543),
+                   CCT)
+
+    return CCT
+ 
+def CCT_to_xy_Kang2002(CCT_arr):
+    """ Kan et al. 2002. Допустимый диапазон: [1667, 25000] """
+
+    # TODO: проверить на всем диапазоне
+    x = np.where(CCT_arr <= 4000,
+                 -0.2661239 * 10 ** 9 / CCT_arr ** 3 -
+                 0.2343589 * 10 ** 6 / CCT_arr ** 2 +
+                 0.8776956 * 10 ** 3 / CCT_arr +
+                 0.179910,
+                 -3.0258469 * 10 ** 9 / CCT_arr ** 3 +
+                 2.1070379 * 10 ** 6 / CCT_arr ** 2 +
+                 0.2226347 * 10 ** 3 / CCT_arr +
+                 0.24039)
+
+    y = np.select([CCT_arr <= 2222,
+                   np.logical_and(CCT_arr > 2222, CCT_arr <= 4000),
+                   CCT_arr > 4000],
+                  [-1.1063814 * x ** 3 -
+                   1.34811020 * x ** 2 +
+                   2.18555832 * x -
+                   0.20219683,
+                   -0.9549476 * x ** 3 -
+                   1.37418593 * x ** 2 +
+                   2.09137015 * x -
+                   0.16748867,
+                   3.0817580 * x ** 3 -
+                   5.8733867 * x ** 2 +
+                   3.75112997 * x -
+                   0.37001483])
+
+    return np.array([x, y])
+
+
+def CCT_to_xyz_Hernandez1999(CCT_arr):
+    """" Конвертирует цветовую температуру в спектр XY """
+
+    return colour.CCT_to_xy(CCT_arr, method="Hernandez1999")
 
 def get_color_CCT(rgb_color):
     """ Возвращает коррелированную цветовую температуру (CCT) цвета RGB (sRGB, [0; 1])
@@ -28,15 +93,16 @@ def get_color_CCT(rgb_color):
     """
     
     # Conversion to tristimulus values.
-    #start1 = time.time()
+    start1 = time.time()
     #XYZ = colour.sRGB_to_XYZ(rgb_color / 255)
-    #end1 = time.time() - start1
+    
 
     rgb = sRGBColor(rgb_color[0], rgb_color[1], rgb_color[2])
     #start2 = time.time()
     xyY = convert_color(rgb, xyYColor, target_illuminant='d65')
     x = xyY.xyy_x
     y = xyY.xyy_y
+    end1 = time.time() - start1
     #end2 = time.time() - start2
 
     # Conversion to chromaticity coordinates.
@@ -69,9 +135,25 @@ def get_image_CCT(rgb_img_array, mask=None):
     # Векторизация вычисления для ускорения.
     #vectfunc = np.vectorize(get_color_CCT, signature="(m,n,3)->(m,n,2)")
     #CCT_arr, bias_arr = vectfunc(rgb_img_array) * mask
-    CCT_bias_mask = np.apply_along_axis(get_color_CCT, -1, rgb_img_array)
-    CCT_arr = CCT_bias_mask[:,:,0]
-    bias_arr = CCT_bias_mask[:,:,1]
+    #CCT_bias_mask = np.apply_along_axis(get_color_CCT, -1, rgb_img_array)
+    #CCT_arr = CCT_bias_mask[:,:,0]
+    #bias_arr = CCT_bias_mask[:,:,1]
+
+    start1 = time.time()
+
+    # Векторищация массива для numpy.map():
+    rgb_vector = rgb_img_array.reshape([x*y, 3])
+    r = rgb_vector[:, 0]
+    g = rgb_vector[:, 1]
+    b = rgb_vector[:, 2]
+    mask = mask.reshape([x*y])
+
+    xyz_arr = np.array(list(map(sRGB_to_xyz, r, g, b)))
+    xy_arr = xyz_arr_to_xy(xyz_arr)
+    end1 = time.time() - start1
+    CCT_arr = CCT_Hernandez1999(xy_arr) * mask
+    x0, y0 = CCT_to_xy_Kang2002(CCT_arr)
+    bias_arr = (((xy_arr[0,:] - x0) ** 2 + (xy_arr[1,:] - y0) ** 2) ** (1 / 2)) * mask
 
     # Вычисление среднего арифметического для коллерированной цветовой температуры изображения и смещения с учетом маски.
     sum_CCTs = np.sum(CCT_arr)
