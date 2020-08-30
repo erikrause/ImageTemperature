3a8239cecc9c628aa5a64e3ffa170fe9656d9786
Эркин Камилов <erkimkamilov@gmail.com>
2020-08-30 05:16:25 +0500

    implemented convertations

--- ImageTemperature/src/image_CCT.py
+++ ImageTemperature/src/image_CCT.py
@@ -2,12 +2,17 @@ import numpy as np
 import colour
 from PIL import Image
 
-from colormath.color_objects import XYZColor, sRGBColor, xyYColor
-from colormath.color_conversions import convert_color
-import time
+#from colormath.color_conversions import 
 
-def sRGB_to_xyz(r,g,b):
-    """ Конвертирует цвет sRGB в XYZ спектр. Math source: http://www.brucelindbloom.com/index.html?Eqn_RGB_XYZ_Matrix.html """
+def __sRGB_to_linear_RGB(rgb):
+    """ Конвертирует массив sRGB в linearRGB. Math source: http://www.brucelindbloom.com/index.html?Eqn_RGB_to_XYZ.html """
+
+    return np.where(rgb[:] <= 0.04045,
+                    rgb/12.92,
+                    ((rgb + 0.055) / 1.055) ** 2.4)
+
+def __linearRGB_to_xyz(r,g,b):
+    """ Конвертирует цвет linearRGB в XYZ спектр. Math source: http://www.brucelindbloom.com/index.html?Eqn_RGB_XYZ_Matrix.html """
 
     M = np.array([[0.4124564, 0.3575761, 0.1804375],
                   [0.2126729, 0.7151522, 0.0721750],
@@ -16,17 +21,18 @@ def sRGB_to_xyz(r,g,b):
 
     return np.dot(M, RGB)
 
-def xyz_arr_to_xy(xyz):
+def __xyz_arr_to_xy(xyz):
 
     sum = np.sum(xyz, axis=-1)
     sum[sum == 0] = 1   # Убирает деление на 0 в дальнейшем
 
+    # TODO debug
     xy_x = xyz[:, 0] / sum
     xy_y = xyz[:, 1] / sum
 
     return np.array([xy_x, xy_y])
 
-def CCT_Hernandez1999(xy_arr):
+def __xy_to_CCT_Hernandez1999(xy_arr):
     """ Возвращает коррерированную цветовую температуру по алгоритму Hernandez et al. 1999 """
 
     n = (xy_arr[0]-0.3366)/(xy_arr[1]-0.1735)
@@ -36,18 +42,19 @@ def CCT_Hernandez1999(xy_arr):
            0.00004 * np.exp(-n / 0.07125))
     #CCT[0] = 77000
 
-    n = np.where(CCT > 50000,
+    prob = CCT > 50000
+    n1 = np.where(CCT > 50000,
                  (xy_arr[0] - 0.3356) / (xy_arr[1] - 0.1691),
                  n)
 
-    CCT = np.where(CCT > 50000,
-                   36284.48953 + 0.00228 * np.exp(-n / 0.07861) +
-                   5.4535e-36 * np.exp(-n / 0.01543),
+    CCT2 = np.where(CCT > 50000,
+                   36284.48953 + 0.00228 * np.exp(-n1 / 0.07861) +
+                   5.4535e-36 * np.exp(-n1 / 0.01543),
                    CCT)
 
     return CCT
  
-def CCT_to_xy_Kang2002(CCT_arr):
+def __CCT_to_xy_Kang2002(CCT_arr):
     """ Kan et al. 2002. Допустимый диапазон: [1667, 25000] """
 
     # TODO: проверить на всем диапазоне
@@ -79,50 +86,10 @@ def CCT_to_xy_Kang2002(CCT_arr):
 
     return np.array([x, y])
 
-
-def CCT_to_xyz_Hernandez1999(CCT_arr):
-    """" Конвертирует цветовую температуру в спектр XY """
-
-    return colour.CCT_to_xy(CCT_arr, method="Hernandez1999")
-
-def get_color_CCT(rgb_color):
-    """ Возвращает коррелированную цветовую температуру (CCT) цвета RGB (sRGB, [0; 1])
-    и смещение (bias) относительно кривой Планка на цветовой плоскости. 
-    
-    rgb_color - numpy массив формата sRGB 
-    """
-    
-    # Conversion to tristimulus values.
-    start1 = time.time()
-    #XYZ = colour.sRGB_to_XYZ(rgb_color / 255)
-    
-
-    rgb = sRGBColor(rgb_color[0], rgb_color[1], rgb_color[2])
-    #start2 = time.time()
-    xyY = convert_color(rgb, xyYColor, target_illuminant='d65')
-    x = xyY.xyy_x
-    y = xyY.xyy_y
-    end1 = time.time() - start1
-    #end2 = time.time() - start2
-
-    # Conversion to chromaticity coordinates.
-    #start3 = time.time()
-    #x, y = colour.XYZ_to_xy(XYZ)
-    #end3 = time.time() - start3
-
-    # Conversion to correlated colour temperature in K.
-    #CCT = colour.temperature.xy_to_CCT([x, y])#, method="Hernandez1999")
-
-    #x0, y0 = colour.CCT_to_xy(CCT)#, method="Hernandez1999")
-    #bias = ((x - x0) ** 2 + (y - y0) ** 2) ** (1 / 2)
-
-    #return np.array([CCT, bias])
-    return np.array([x, y])
-
 def get_image_CCT(rgb_img_array, mask=None):
     """ Возвращает среднее значение коллерированной цветовой температуры изображения и смещения.
     
-    rgb_img_array - numpy массив пикселей формата sRGB в диапазоне [0; 255];
+    rgb_img_array - numpy массив пикселей формата sRGB в диапазоне [0; 1];
     mask - numpy массив маски. Значение маски работает как множитель для каждого пикселя при расчете его значения CCT.
     """
     x, y, _ = rgb_img_array.shape
@@ -130,29 +97,27 @@ def get_image_CCT(rgb_img_array, mask=None):
     if mask == None:
         mask = np.ones([x, y])
     else:
-        mask = normalize_mask(mask)
+        mask = __normalize_mask(mask)
 
     # Векторизация вычисления для ускорения.
     #vectfunc = np.vectorize(get_color_CCT, signature="(m,n,3)->(m,n,2)")
-    #CCT_arr, bias_arr = vectfunc(rgb_img_array) * mask
-    #CCT_bias_mask = np.apply_along_axis(get_color_CCT, -1, rgb_img_array)
-    #CCT_arr = CCT_bias_mask[:,:,0]
-    #bias_arr = CCT_bias_mask[:,:,1]
-
-    start1 = time.time()
 
     # Векторищация массива для numpy.map():
     rgb_vector = rgb_img_array.reshape([x*y, 3])
-    r = rgb_vector[:, 0]
-    g = rgb_vector[:, 1]
-    b = rgb_vector[:, 2]
     mask = mask.reshape([x*y])
 
-    xyz_arr = np.array(list(map(sRGB_to_xyz, r, g, b)))
-    xy_arr = xyz_arr_to_xy(xyz_arr)
-    end1 = time.time() - start1
-    CCT_arr = CCT_Hernandez1999(xy_arr) * mask
-    x0, y0 = CCT_to_xy_Kang2002(CCT_arr)
+    linearRGB = __sRGB_to_linear_RGB(rgb_vector)
+
+    r = linearRGB[:, 0]
+    g = linearRGB[:, 1]
+    b = linearRGB[:, 2]
+    
+    xyz_arr = np.array(list(map(__linearRGB_to_xyz, r, g, b)))   # Конвертация в XYZ
+    xy_arr = __xyz_arr_to_xy(xyz_arr)       # Конвертация в XY
+    CCT_arr = __xy_to_CCT_Hernandez1999(xy_arr) * mask      # Получение температуры для каждого пикселя
+
+    # Вычисление смещения от кривой планка на плоскости XY
+    x0, y0 = __CCT_to_xy_Kang2002(CCT_arr)      
     bias_arr = (((xy_arr[0,:] - x0) ** 2 + (xy_arr[1,:] - y0) ** 2) ** (1 / 2)) * mask
 
     # Вычисление среднего арифметического для коллерированной цветовой температуры изображения и смещения с учетом маски.
@@ -165,7 +130,7 @@ def get_image_CCT(rgb_img_array, mask=None):
 
     return np.array([mean_CCT, mean_bias])
 
-def normalize_mask(mask):
+def __normalize_mask(mask):
     """ Нормализует маску в диапазон [0; 1]
     
     mask - numpy массив маски.
--- Test/test_imageCCT.py
+++ Test/test_imageCCT.py
@@ -1,17 +1,21 @@
 import unittest
 import numpy as np
 from src import image_CCT
+from src.image_CCT import __linearRGB_to_xyz as _image_CCT__linearRGB_to_xyz
+from src.image_CCT import __xyz_arr_to_xy as _image_CCT__xyz_arr_to_xy
+from src.image_CCT import __xy_to_CCT_Hernandez1999 as _image_CCT__xy_to_CCT_Hernandez1999
+from src.image_CCT import __sRGB_to_linear_RGB as _image_CCT__sRGB_to_linear_RGB
+
 import math
+from PIL import Image
+
+import colour
+import colormath
+import colormath.color_conversions
+from colormath.color_objects import XYZColor, sRGBColor, xyYColor
+
 
 class Test_image_CCT(unittest.TestCase):
-
-    def test_white_color_CCT(self):
-        """ Тест температуры и смещения для белого цвета с погрешностью 5% """
-
-        CCT, bias = image_CCT.get_color_CCT(np.array([1, 1, 1]))
-
-        self.assertTrue((CCT > 6200) and (CCT < 6800), msg="Цветовая температура белого пикселя за пределами погрешности")
-        self.assertTrue((bias < math.fabs(0.05)), msg="Смещение XY белого пикселя за пределами погрешности")
 
     def test_normalize_mask(self):
         """ Тест нормализации маски. """
@@ -31,6 +35,41 @@ class Test_image_CCT(unittest.TestCase):
         self.assertTrue((CCT > 6200) and (CCT < 6800), msg="Цветовая температура белого изображения за пределами погрешности")
         self.assertTrue((bias < math.fabs(0.05)), msg="Смещение XY белого изображения за пределами погрешности")
 
+    def test_CCT_by_mean_pixel(self):
+
+        img = Image.open("l6400.png")
+
+    def test_xy(self):
+
+        rgb = np.array([0.0, 0.0039215686274509803, 0.011764705882352941])
+        r, g, b = _image_CCT__sRGB_to_linear_RGB(rgb)
+
+        xyz = _image_CCT__linearRGB_to_xyz(r,g,b)
+        rgb_obj = sRGBColor(rgb[0], rgb[1], rgb[2])
+        xyz2 = colormath.color_conversions.RGB_to_XYZ(rgb_obj)
+        xyz3 = colour.sRGB_to_XYZ(rgb)
+
+        #x = 0.18724231852549086	
+        #y = 0.1940723233616907
+
+        xy = _image_CCT__xyz_arr_to_xy(np.expand_dims(xyz, 0))
+        #xy2 = _image_CCT__xyz_arr_to_xy(np.swapaxes(np.array([[xyz2.xyz_x], [xyz2.xyz_y], [xyz2.xyz_z]]), 0, -1))
+        xy2 = colormath.color_conversions.XYZ_to_xyY(xyz2)
+        xy3 = colour.XYZ_to_xy(xyz3)
+
+        CCT = _image_CCT__xy_to_CCT_Hernandez1999(xy)
+        #CCT2 = _image_CCT__xy_to_CCT_Hernandez1999(xy2)
+
+        x = xy[0,0]
+        y = xy[1,0]
+        real = colour.temperature.xy_to_CCT_Hernandez1999(np.array([x, y]))
+        real3 = colour.temperature.xy_to_CCT_Hernandez1999(xy3)
+        real4 = colour.temperature.xy_to_CCT_McCamy1992(xy3)
+        real5 = colour.temperature.xy_to_CCT_Kang2002(xy3)
+
+        obr = colour.temperature.CCT_to_xy_Hernandez1999(xy3)
+
+        prob = 0
 
 if __name__ == '__main__':
     unittest.main()
